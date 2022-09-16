866aeafacc1eb297313e88f841ff9ad6f29a5de2
Valentin92257 <112700750+Valentin92257@users.noreply.github.com>
2022-09-16 13:39:54 +0300

    1.0

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -69,7 +69,7 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
 fun ageDescription(age: Int): String {
-    var age1 :Int
+    val age1 :Int
     if (age>=100)
         age1=age-100
     else
