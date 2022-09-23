746d63c3f61f2a959f5ac877cb3c928d91155b77
Valentin92257 <112700750+Valentin92257@users.noreply.github.com>
2022-09-23 16:52:01 +0300

    3.5

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -160,11 +160,17 @@ fun collatzSteps(x: Int): Int {
  * минимальное число k, которое делится и на m и на n без остатка
  */
 fun lcm(m: Int, n: Int): Int {
-    var i = max(m, n)
-    while (m % i != 0 || n % i != 0) {
-        i--
+    var a = m
+    var b = n
+    while(a != 0 && b != 0){
+        if(a > b){
+            a%=b
+        }
+        else{
+            b%=a
+        }
     }
-    return m * n / i
+    return m*n/(a+b)
 }
 
 
@@ -175,14 +181,9 @@ fun lcm(m: Int, n: Int): Int {
  * Взаимно простые числа не имеют общих делителей, кроме 1.
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
-fun isCoPrime(m: Int, n: Int): Boolean {
-    for (i in 2..min(m, n)) {
-        if (m % i == 0 && n % i == 0) {
-            return false
-        }
-    }
-    return true
-}
+fun isCoPrime(m: Int, n: Int): Boolean =
+    m*n/lcm(m,n) == 1
+
 
 /**
  * Средняя (3 балла)
