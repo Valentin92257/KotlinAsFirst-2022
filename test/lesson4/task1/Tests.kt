96204481a7089a45e2a7d40687791c7e71b0a9df
Valentin92257 <112700750+Valentin92257@users.noreply.github.com>
2022-10-14 15:34:20 +0300

    4.2

--- test/lesson4/task1/Tests.kt
+++ test/lesson4/task1/Tests.kt
@@ -210,7 +210,7 @@ class Tests {
     @Test
     @Tag("4")
     fun decimalFromString() {
-        assertEquals(1, decimalFromString("1", 2))
+        assertEquals(3, decimalFromString("11", 2))
         assertEquals(100, decimalFromString("1210", 4))
         assertEquals(250, decimalFromString("13c", 14))
         assertEquals(1000, decimalFromString("2ec", 19))
