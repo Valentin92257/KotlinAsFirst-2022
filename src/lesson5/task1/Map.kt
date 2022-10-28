2e46aba546fbe215ef053f95dcad2879502fc8fa
Valentin92257 <112700750+Valentin92257@users.noreply.github.com>
2022-10-28 15:21:44 +0300

    4.9

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -396,9 +396,10 @@ fun bagPacking(treasures: Map<String, Pair<Int, Int>>, capacity: Int): Set<Strin
     }
     num1 = num + (treasures[b1.first]?.first ?: 0)
     for ((a, b) in list1) {
-        if (num - (treasures[a]?.first ?: 0) >= 0 && !fin.contains(a)) {
+        if (num1 - (treasures[a]?.first ?: 0) >= 0 && !fin.contains(a)) {
             num1 = min(num1, num - (treasures[a]?.first ?: 0))
             fin.remove(b1.first)
+            fin.add(a)
             b1 = Pair(a, b)
         }
     }
